52df4c7732466c1a517bf9889e22657a3c000041
Heverton Reis <heverton.reis04@gmail.com>
2022-09-30 22:03:22 -0300

    doc: Adicionando arquivo ignore

--- ATV_02/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/heverton/ROS_Workspaces/ATV_02/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
