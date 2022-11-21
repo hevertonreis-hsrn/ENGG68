bf350bf13dab3929c572ccb50d9c9aca7fb4b876
Heverton Reis <heverton.reis04@gmail.com>
2022-11-20 22:12:57 -0300

    doc: Iniciando ambiente do projeto final

---
+++ ATV_03/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/heverton/ROS_Workspaces/ENGG68/ATV_03/src
+# Build directory: /home/heverton/ROS_Workspaces/ENGG68/ATV_03/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lar_gazebo")
---
+++ ATV_03/build/lar_gazebo/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/heverton/ROS_Workspaces/ENGG68/ATV_03/src/lar_gazebo
+# Build directory: /home/heverton/ROS_Workspaces/ENGG68/ATV_03/build/lar_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_lar_gazebo_roslaunch-check_src_launch "/home/heverton/ROS_Workspaces/ENGG68/ATV_03/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/home/heverton/ROS_Workspaces/ENGG68/ATV_03/build/test_results/lar_gazebo/roslaunch-check_src_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/heverton/ROS_Workspaces/ENGG68/ATV_03/build/test_results/lar_gazebo" "/opt/ros/melodic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/heverton/ROS_Workspaces/ENGG68/ATV_03/build/test_results/lar_gazebo/roslaunch-check_src_launch.xml\" \"/home/heverton/ROS_Workspaces/ENGG68/ATV_03/src/lar_gazebo/src/launch\" ")
